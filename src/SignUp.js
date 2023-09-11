1d698eed48f0481a370f4180a183c172e3522799
akorbutovski <akorbutovski@elinext.com>
2023-09-11 15:18:49 +0300

    forms: added all controlled fields

--- src/SignUp.js
+++ src/SignUp.js
@@ -6,14 +6,17 @@ import {IconAt} from "@tabler/icons-react";
 function SignIn() {
     const [name,  setName] = useState('Your name')
     const [full,  setFull] = useState('Full name')
+    const [frame,  setFrame] = useState('react')
     const [asterix,  setAsterix] = useState(false)
     const [disabled,  setDisabled] = useState(false)
+    const [radius,  setRadius] = useState(25)
+    const [size,  setSize] = useState(50)
 
     return (
         <>
             <TextInput
                 placeholder="Your e-mail"
-                label="Your e-mail"
+                label="Your e-mail" size={size} radius={radius / 5}
                 withAsterisk={asterix} disabled={disabled}
                 icon={<IconAt size="1rem" />} classNames='.mantine-Slider-root'
             />
@@ -35,15 +38,34 @@ function SignIn() {
                 placeholder="Error"
                 label="Error" disabled
             />
-            <Select data={['Default']}
+            <Select data={[
+                { value: 'react', label: 'React' },
+                { value: 'ng', label: 'Angular' },
+                { value: 'svelte', label: 'Svelte' },
+                { value: 'vue', label: 'Vue' },
+            ]}
+                    defaultValue='React' value={frame}
                 placeholder="Variant"
-                label="Variant"
-                    // type="number" defaultValue='Default' classNames={{ input: '.mantine-TextInput-input' }}
+                label="Variant" onChange={(item) => setFrame(item)}
             />
-            <Slider on
+            <div className="mantine-Text-root"> Radius </div>
+            {/*<Slider onChange={(event) => setRadius(event.target.checked)}*/}
+            <Slider onChange={(value) => setRadius(value)} value={radius}
+                    marks={[
+                        { value: 0, label: 'xs' },
+                        { value: 25, label: 'sm' },
+                        { value: 50, label: 'md' },
+                        { value: 75, label: 'lg' },
+                        { value: 100, label: 'xl' },
+                    ]}
+            />
+            <div className="mantine-Text-root"> Size </div>
+            <Slider onChange={(e) => setSize(e)} value={size}
+            /*<Slider onChange={(e) => console.log(e) } value={size}*/
+            /*<Slider onChange={setSize} value={size}*/
                 marks={[
                     { value: 20, label: '20%' },
-                    { value: 50, label: '50%' },
+                    { value: 40, label: '40%' },{ value: 60, label: '60%' },
                     { value: 80, label: '80%' },
                 ]}
             />
